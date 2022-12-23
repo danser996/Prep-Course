1a29d8826fd65e46551c618cf48e9a77fc461f4d
danser996 <daniel_serna01152@elpoli.edu.co>
2022-12-23 09:29:27 -0500

    avance_2_js4

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -21,8 +21,9 @@ function agregarPropiedad (objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
-  objeto.property = null;
-  return objeto
+  const prop = property;
+  objeto[prop] = null;
+  return objeto;
 }
 
 function invocarMetodo (objeto, metodo) {
