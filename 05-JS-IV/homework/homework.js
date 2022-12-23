6ea9ac8df593012a1bdd4145197a3540667ef791
danser996 <daniel_serna01152@elpoli.edu.co>
2022-12-23 10:34:52 -0500

    avance_js4

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -78,6 +78,10 @@ function tienePropiedad (objeto, propiedad) {
   // "propiedad" es un string
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (objeto['propiedad'] === undefined){
+    return false
+  }
+  return true
 }
 
 function verificarPassword (usuario, password) {
