c386ce4ae4b82466ea3762c66c97380c8c4bd3b3
danser996 <daniel_serna01152@elpoli.edu.co>
2022-12-23 19:18:31 -0500

    avance_js5

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -35,12 +35,13 @@ function agregarStringInvertida() {
   // Ej: 'menem'.reverse() => menem
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
-  String.prototype.reverse = function(){
-    let StringInvertida = ''
-    for(let i = this.length - 1;i  >= 0; i--){
-      stringInvertida = stringInvertida + this.charAt(i)
-    } return stringInvertida
-  }
+  String.prototype.reverse = function() {
+    var stringInvertida = '';
+    for(var i = this.length - 1; i>=0; i--) {
+      stringInvertida = stringInvertida + this.charAt(i);
+    }
+    return stringInvertida;
+  };
   
 }
 
