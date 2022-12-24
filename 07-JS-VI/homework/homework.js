7e81a49ce77d0c500d5e28331c7661e3d25c0a02
danser996 <daniel_serna01152@elpoli.edu.co>
2022-12-23 20:32:48 -0500

    avance_js6

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -41,7 +41,9 @@ function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
-  cb(array.forEach(Elemento=> Elemento))
+  for(let i = 0; i < array.length; i++){
+    cb(array[i])
+  }
 }
 
 function map(array, cb) {
